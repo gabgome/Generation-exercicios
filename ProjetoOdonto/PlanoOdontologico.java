834a56fc6a05facaf4c6cc9233e2ba8667f67285
unknown <gferoligomes@hotmail.com>
2020-09-24 15:37:05 -0300

    exercicios

---
+++ ProjetoOdonto/PlanoOdontologico.java
@@ -0,0 +1,44 @@
+package Odonto;
+
+public class PlanoOdontologico {
+	private String nome;
+	private String email;
+	private int idade;
+	private int quantidade;
+	
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+}
